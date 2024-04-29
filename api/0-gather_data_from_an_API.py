1418deffa5f3d850de46b10b16ed3f9951c72ac6
Juanse1595 <jspbescos15@gmail.com>
2024-04-28 22:45:50 -0500

    UPD 1rst task fixed some errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,18 +4,21 @@
 import requests
 from sys import argv
 
-employeeID = argv[1]
-employeeData = requests.get(f'https://jsonplaceholder.typicode.com/users/{employeeID}')
-employeeTODOs = requests.get(f'https://jsonplaceholder.typicode.com/users/{employeeID}/todos').json()
-employeeName = employeeData.json()["name"]
-completedTasks = []
-countCompletedTasks = 0
-totalTasks = len(employeeTODOs)
-for todo in employeeTODOs:
-    if todo["completed"] == True:
-        countCompletedTasks += 1
-        completedTasks.append(todo["title"])
-print("Employee {} is done with tasks({}/{}):".format(employeeName, countCompletedTasks, totalTasks))
-for task in completedTasks:
-    print("\t{}".format(task))
-
+if __name__ == "__main__":
+    employeeID = argv[1]
+    employeeData = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employeeID}')
+    employeeTODOs = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employeeID}/todos').json()
+    employeeName = employeeData.json()["name"]
+    completedTasks = []
+    countCompletedTasks = 0
+    totalTasks = len(employeeTODOs)
+    for todo in employeeTODOs:
+        if todo["completed"] is True:
+            countCompletedTasks += 1
+            completedTasks.append(todo["title"])
+    print("Employee {} is done with tasks({}/{}):".format(
+        employeeName, countCompletedTasks, totalTasks))
+    for task in completedTasks:
+        print("\t{}".format(task))
